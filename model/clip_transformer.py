8b72ba5ab80684f217043af6278c485952611ed8
Rahul <64926002+RAHUL13-13@users.noreply.github.com>
2024-02-21 22:18:44 +0530

    Update clip_transformer.py

--- model/clip_transformer.py
+++ model/clip_transformer.py
@@ -23,6 +23,7 @@ class CLIPTransformer(nn.Module):
         
         config.pooling_type = 'transformer'
         self.pool_frames = Transformer(config)
+        blip = DataParallel(model, device_ids=self.device_ids)
     
     def forward(self, data, return_all_frames=False):
         batch_size = data['video'].shape[0]
@@ -36,7 +37,6 @@ class CLIPTransformer(nn.Module):
         
         model = self.blip.to(torch.device('cuda:2'))
         
-        blip = DataParallel(model, device_ids=self.device_ids)
         video_features = blip(video_data)
         
         # print(len(text_data['input_ids']), video_data.shape)
